7961a029e8524e199291f23534d698c989f4bb46
Nguyễn Văn Trường <nguyenvantruong@MacBook-Air-cua-Nguyen.local>
2026-07-14 19:32:40 +0700

    Implement module login

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,26 +5,23 @@ export class LoginPage {
     readonly usernameInput: Locator;
     readonly passwordInput: Locator;
     readonly loginButton: Locator;
+    readonly errorMessage: Locator;
 
     constructor(page: Page) {
         this.page = page;
-        this.usernameInput = page.locator('#user-name');
-        this.passwordInput = page.locator('#password');
-        this.loginButton = page.locator('#login-button');
+        this.usernameInput = page.getByPlaceholder('Username');
+        this.passwordInput = page.getByPlaceholder('Password');
+        this.loginButton = page.getByRole('button', { name: 'Login' });
+        this.errorMessage = page.locator('[data-test="error"]');
     }
 
     async navigateLogin() {
-        await this.page.goto('https://www.saucedemo.com/');
+        await this.page.goto('/');
     }
 
     async login(username: string, password: string) {
         await this.usernameInput.fill(username);
         await this.passwordInput.fill(password);
         await this.loginButton.click();
-    }
-
-    async getErrorMessage(){
-        const errorMessageLocator = this.page.locator('.error-message-container h3');
-        return await errorMessageLocator.textContent();
     }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,22 +3,51 @@ import { LoginPage } from '../pages/LoginPage';
 import testData from '../test-data/users.json';
 
 test.describe('SauceDemo Login Tests', () => {
-    test('should login successfully with valid credentials', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        await loginPage.navigateLogin();
-        await loginPage.login(testData.validUser.username, testData.validUser.password);
+    let loginPage: LoginPage;
 
-        // Verify successful login by checking for the presence of the inventory page
-        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        await loginPage.navigateLogin();
     });
 
-    test('should show error message for locked out users', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        await loginPage.navigateLogin();
-        await loginPage.login(testData.lockedUser.username, testData.lockedUser.password);
-        // Verify error message is displayed
-        const errorMessage = page.locator('.error-message-container');
-        await expect(errorMessage).toBeVisible();
-        await expect(errorMessage).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+    test.describe('SauceDemo Login Feature - Positive Tests', () => {
+        test('should login successfully with valid credentials', async ({ page }) => {
+            await loginPage.login(testData.validUser.username, testData.validUser.password);
+
+            // Verify successful login by checking for the presence of the inventory page
+            await expect(page).toHaveURL(/inventory\.html/);
+        });
+    });
+
+    test.describe('SauceDemo Login Feature - Negative Tests', () => {
+        test('should display error message for invalid credentials', async ({ page }) => {
+            await loginPage.login(testData.invalidUser.username, testData.invalidUser.password);
+
+            // Verify error message is displayed
+            await expect(loginPage.errorMessage).toContainText('Epic sadface: Username and password do not match any user in this service');
+        });
+    });
+    test.describe('SauceDemo Login Feature - Edge Case Tests', () => {
+        test('should display error message for empty username and password', async ({ page }) => {
+            await loginPage.login('', '');
+
+            // Verify error message is displayed
+            await expect(loginPage.errorMessage).toContainText('Epic sadface: Username is required');
+        });
+
+        test('should display error message for empty password', async ({ page }) => {
+            await loginPage.login(testData.validUser.username, '');
+
+            // Verify error message is displayed
+            await expect(loginPage.errorMessage).toContainText('Epic sadface: Password is required');
+        });
+    });
+    test.describe('SauceDemo Login Feature - Login with locked out user', () => {
+        test('should display error message for locked out user', async ({ page }) => {
+            await loginPage.login(testData.lockedUser.username, testData.lockedUser.password);
+
+            // Verify error message is displayed
+            await expect(loginPage.errorMessage).toContainText('Epic sadface: Sorry, this user has been locked out.');
+        });
     });
 });
